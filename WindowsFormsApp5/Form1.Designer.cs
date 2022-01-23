4de4eeabf5873d4c30062a003baef0f6f809fe90
nikolayarti <nikolayarti@yandex.by>
2022-01-23 10:17:10 +0200

    commit

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.Enemy1 = new System.Windows.Forms.PictureBox();
+            this.Enemy3 = new System.Windows.Forms.PictureBox();
+            this.Bullet = new System.Windows.Forms.PictureBox();
+            this.Enemy2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bullet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(214, 359);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 34);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Score: 0";
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.Transparent;
+            this.Player.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Player.Image = global::WindowsFormsApp5.Properties.Resources.Player;
+            this.Player.Location = new System.Drawing.Point(246, 619);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(60, 106);
+            this.Player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Player.TabIndex = 13;
+            this.Player.TabStop = false;
+            // 
+            // Enemy1
+            // 
+            this.Enemy1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.Enemy1.Image = ((System.Drawing.Image)(resources.GetObject("Enemy1.Image")));
+            this.Enemy1.Location = new System.Drawing.Point(69, 33);
+            this.Enemy1.Name = "Enemy1";
+            this.Enemy1.Size = new System.Drawing.Size(73, 52);
+            this.Enemy1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Enemy1.TabIndex = 11;
+            this.Enemy1.TabStop = false;
+            // 
+            // Enemy3
+            // 
+            this.Enemy3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.Enemy3.Image = ((System.Drawing.Image)(resources.GetObject("Enemy3.Image")));
+            this.Enemy3.Location = new System.Drawing.Point(397, 33);
+            this.Enemy3.Name = "Enemy3";
+            this.Enemy3.Size = new System.Drawing.Size(73, 52);
+            this.Enemy3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Enemy3.TabIndex = 10;
+            this.Enemy3.TabStop = false;
+            // 
+            // Bullet
+            // 
+            this.Bullet.Image = global::WindowsFormsApp5.Properties.Resources.Bullet__1_;
+            this.Bullet.Location = new System.Drawing.Point(272, 563);
+            this.Bullet.Name = "Bullet";
+            this.Bullet.Size = new System.Drawing.Size(13, 18);
+            this.Bullet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Bullet.TabIndex = 9;
+            this.Bullet.TabStop = false;
+            // 
+            // Enemy2
+            // 
+            this.Enemy2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.Enemy2.Image = ((System.Drawing.Image)(resources.GetObject("Enemy2.Image")));
+            this.Enemy2.Location = new System.Drawing.Point(229, 33);
+            this.Enemy2.Name = "Enemy2";
+            this.Enemy2.Size = new System.Drawing.Size(73, 52);
+            this.Enemy2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Enemy2.TabIndex = 6;
+            this.Enemy2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(573, 737);
+            this.Controls.Add(this.Player);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Enemy1);
+            this.Controls.Add(this.Enemy3);
+            this.Controls.Add(this.Bullet);
+            this.Controls.Add(this.Enemy2);
+            this.ForeColor = System.Drawing.Color.Yellow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Alien Invasion";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.OnKeyUP);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bullet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox Enemy2;
+        private System.Windows.Forms.PictureBox Bullet;
+        private System.Windows.Forms.PictureBox Enemy3;
+        private System.Windows.Forms.PictureBox Enemy1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox Player;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Drawing;
+using System.Media;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        float animTime;
+        int hardTime;
+        bool right, left, up, gameOver;
+        int score;
+        int playerSpeed = 12;
+        int enemySpeed = 4;
+        int bulletSpeed = 17;
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            reset();
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                left = true;
+            }
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                right = true;
+            }
+
+
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            animTime++;
+            if (animTime >= 200)
+            {
+                animTime = 0;
+                if(Player.Image == Properties.Resources.Fly)
+                {
+                    Player.Image = Properties.Resources.Player;
+                }
+                else if(Player.Image == Properties.Resources.Player)
+                {
+                    Player.Image = Properties.Resources.Fly;
+                }
+            }
+            
+            hardTime++;
+            if(hardTime >= 500)
+            {
+                hardTime = 0;
+                enemySpeed++;
+                if(enemySpeed >= 13)
+                {
+                    enemySpeed = 8;
+                }
+            }
+            if (left == true && Player.Left > 12)
+            {
+                Player.Left -= playerSpeed;
+            }
+            if (right == true && Player.Left < 500)
+            {
+                Player.Left += playerSpeed;
+            }
+
+            Enemy1.Top += enemySpeed;
+            Enemy2.Top += enemySpeed;
+            Enemy3.Top += enemySpeed;
+
+            if(Enemy1.Top > 710 || Enemy2.Top > 710 || Enemy3.Top > 710)
+            {
+                GameOver();
+            }
+
+            if (up)
+            {
+                bulletSpeed = 34;
+                Bullet.Top -= bulletSpeed;
+            }
+            else
+            {
+                Bullet.Left = -300;
+            }
+
+            if(Bullet.Top <= -30)
+            {
+                up = false;
+            }
+            if (Bullet.Bounds.IntersectsWith(Enemy1.Bounds))
+            {
+                Enemy1.Top = -450;
+                score += 10;
+                Enemy1.Left = rnd.Next(12, 500);
+                up = false;
+                label1.Text = "Score: " + score.ToString();
+                SoundPlayer sp = new SoundPlayer(Properties.Resources.Hit);
+                sp.Play();
+            }
+            else if (Bullet.Bounds.IntersectsWith(Enemy2.Bounds))
+            {
+                Enemy2.Top = -450;
+                score += 10;
+                Enemy2.Left = rnd.Next(12, 500);
+                up = false;
+                label1.Text = "Score: " + score.ToString();
+                SoundPlayer sp = new SoundPlayer(Properties.Resources.Hit);
+                sp.Play();
+            }
+            else if (Bullet.Bounds.IntersectsWith(Enemy3.Bounds))
+            {
+                Enemy3.Top = -450;
+                score += 10;
+                Enemy3.Left = rnd.Next(12, 500);
+                up = false;
+                label1.Text = "Score: " + score.ToString();
+                SoundPlayer sp = new SoundPlayer(Properties.Resources.Hit);
+                sp.Play();
+            }
+            
+        }
+
+        private void OnKeyUP(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                left = false;
+            }
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                right = false;
+            }
+            if(e.KeyCode == Keys.Space && up == false)
+            {
+                up = true;
+                Bullet.Top = Player.Top + 10;
+                Bullet.Left = Player.Left + (Player.Width / 2);
+                SoundPlayer sp = new SoundPlayer(Properties.Resources.Shoot);
+                sp.Play();
+            }
+
+            if(e.KeyCode == Keys.Enter && gameOver)
+            {
+                reset();
+            }
+        }
+
+
+
+        private void reset()
+        {
+            hardTime = 0;
+            enemySpeed = 4;
+            timer1.Start();
+            Enemy1.Left = rnd.Next(12, 458);
+            Enemy2.Left = rnd.Next(12, 458);
+            Enemy3.Left = rnd.Next(12, 458);
+
+            Enemy1.Top = rnd.Next(60, 200) * -1;
+            Enemy2.Top = rnd.Next(60, 300) * -1;
+            Enemy3.Top = rnd.Next(60, 500) * -1;
+            score = 0;
+            bulletSpeed = 0;
+            Bullet.Top = -300;
+            label1.Text = "Score: " + score.ToString();
+            up = false;
+        }
+
+        private void GameOver()
+        {
+            SoundPlayer sp = new SoundPlayer(Properties.Resources.GameOver);
+            sp.Play();
+            gameOver = true;
+            timer1.Stop();
+            label1.Text += Environment.NewLine + "Game Over!" + Environment.NewLine + "Enter to restart";
+
+        }
+
+    }
+}
